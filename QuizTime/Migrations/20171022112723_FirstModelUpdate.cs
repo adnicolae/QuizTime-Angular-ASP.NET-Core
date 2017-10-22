e4eb2716358e479cef3e21c1f90251f0c482d587
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-10-22 12:27:49 +0100

    Added the updated migration.

---
+++ QuizTime/Migrations/20171022112723_FirstModelUpdate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace QuizTime.Migrations
+{
+    public partial class FirstModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ChoiceId",
+                table: "Results",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_ChoiceId",
+                table: "Results",
+                column: "ChoiceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Choices_ChoiceId",
+                table: "Results",
+                column: "ChoiceId",
+                principalTable: "Choices",
+                principalColumn: "ChoiceId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Choices_ChoiceId",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_ChoiceId",
+                table: "Results");
+
+            migrationBuilder.DropColumn(
+                name: "ChoiceId",
+                table: "Results");
+        }
+    }
+}
