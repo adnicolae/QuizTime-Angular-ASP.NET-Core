6039ef209813c4cd2957aae882f3bba17896c2f4
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-11-21 21:23:48 +0000

    Add migration to reflect model updates.

--- QuizTime/Migrations/20171121205451_QuizAndSessionModelsUpdateMigration.Designer.cs
+++ QuizTime/Migrations/20171121205451_QuizAndSessionModelsUpdateMigration.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace QuizTime.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20171121205451_QuizAndSessionModelsUpdateMigration")]
+    partial class QuizAndSessionModelsUpdateMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ QuizTime/Migrations/20171121205451_QuizAndSessionModelsUpdateMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace QuizTime.Migrations
+{
+    public partial class QuizAndSessionModelsUpdateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeLimit",
+                table: "Sessions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeducedPoints",
+                table: "Quizzes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeLimit",
+                table: "Quizzes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeducedPoints",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "TimeLimit",
+                table: "Quizzes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeLimit",
+                table: "Sessions",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
