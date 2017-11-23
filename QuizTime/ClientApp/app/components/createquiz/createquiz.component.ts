a906fa4c180eb8ad0e72d62f4ac4f677706366f7
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-11-23 22:54:05 +0000

    BUGFIX: Multiple requests are sent correctly.

--- QuizTime/ClientApp/app/components/createquiz/createquiz.component.ts
+++ QuizTime/ClientApp/app/components/createquiz/createquiz.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Repository } from '../../data/repository';
 import { Quiz } from '../../models/quiz.model';
@@ -7,17 +7,20 @@ import { Injectable, Inject } from "@angular/core";
 import { Http, RequestMethod, Request, Response } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/takeWhile";
 import { Router } from "@angular/router";
 
 @Component({
     selector: 'create-quiz',
     templateUrl: './createquiz.component.html'
 })
-export class CreateQuizComponent {
+export class CreateQuizComponent implements OnDestroy{
     quizForm: FormGroup;
     latestQuiz: Quiz;
     quizTimeLimit: number = 30;
     private thisBaseUrl: string;
+    errorMessage = "";
+    private alive: boolean = true;
 
     constructor(private router: Router, private formBuilder: FormBuilder, private repo: Repository, private http: Http, @Inject('BASE_URL') baseUrl: string) {
         this.thisBaseUrl = baseUrl;
@@ -34,22 +37,15 @@ export class CreateQuizComponent {
         return this.quizForm.get('choices') as FormArray;
     }
 
-    get quizzes(): Quiz[] {
-        return this.repo.quizzes;
-    }
-
-    addChoice() {
-        this.choices.push(this.formBuilder.group(new Choice()));
-    }
-
-    removeChoice(index: number) {
-        this.choices.removeAt(index);
+    ngOnDestroy() {
+        this.alive = false;
+        this.repo.alive = false;
     }
 
     onSubmit() {
         const formModel = this.quizForm.value;
 
-        const saveQuiz: Quiz = {
+        const newQuiz: Quiz = {
             quizId: 0,
             title: formModel.title as string,
             timeLimit: this.quizTimeLimit,
@@ -59,54 +55,40 @@ export class CreateQuizComponent {
             creator: this.repo.quizzes[1].creator
         };
 
-        this.repo.createQuiz(saveQuiz); // add quiz to db and repository
+        let data = {
+            title: newQuiz.title,
+            timeLimit: newQuiz.timeLimit,
+            assignedPoints: newQuiz.assignedPoints,
+            deducedPoints: newQuiz.deducedPoints,
+            dateCreated: newQuiz.dateCreated,
+            creator: newQuiz.creator ? newQuiz.creator.userId : 0
+        };
 
-        console.log(this.quizzes); // shows newly added quiz
-        //this.wait(5000);
-        console.log();
-        var l = this.quizzes.length;
-        console.log(l);
-        console.log(this.repo.quizzes[this.repo.quizzes.length - 1]);
-        console.log(this.getLastElement()); // gets the length-2 instead of -1 (penultimate)
-        console.log(this.quizzes.slice(-1));
-    }
-
-    getLastElement() {
-        return this.quizzes[this.quizzes.length - 1];
-    }
-
-    wait(ms: number) {
-        var start = new Date().getTime();
-        var end = start;
-        while (end < start + ms) {
-            end = new Date().getTime();
-        }
-    }
-
-    addChoices() {
-        const formModel = this.quizForm.value;
-        console.log(this.repo.quizzes[this.repo.quizzes.length - 1]);
-
-        const ch: any = formModel.choices.map((choice: Choice) => choice.choiceId = 0);
-
-        const ch2: any = formModel.choices.map((choice: Choice) => choice.quiz = this.repo.quizzes[this.repo.quizzes.length - 1]);
-
-        const choicesCopy: Choice[] = formModel.choices.map(
-            (choice: Choice) => Object.assign(new Choice(), choice));
-        
-        for (var c in choicesCopy) {
-            //this.repo.createChoice(choicesCopy[c]);
-            var idx = parseInt(c) + 1;
-            choicesCopy.map((choice: Choice) => choice.title = "Choice " + idx.toString());
-            console.log(choicesCopy[c]);
-            this.repo.createChoice(choicesCopy[c]);
-        }
-
-        this.router.navigateByUrl("/new/quiz/created" );
-    }
-
-    showLastQuiz() {
-        console.log(this.repo.quizzes[this.repo.quizzes.length - 1]);
+        this.http
+            .post(`${this.repo.urlBase}/api/quizzes`, data)
+            .takeWhile(() => this.alive)
+            .subscribe(response => {
+                console.log("New quiz with id: " + response.json());
+                newQuiz.quizId = response.json();
+                console.log(newQuiz);
+                this.repo.quizzes.push(newQuiz);
+                const ch: any = formModel.choices
+                    .map((choice: Choice) => choice.choiceId = 0);
+                const ch2: any = formModel.choices
+                    .map((choice: Choice) => choice.quiz = newQuiz);
+                const choicesCopy: Choice[] = formModel.choices.map(
+                    (choice: Choice) => Object.assign(new Choice(), choice));
+                for (var c in choicesCopy) {
+                    var idx = parseInt(c) + 1;
+                    choicesCopy.map((choice: Choice) => choice.title = "Choice " + idx.toString());
+                    this.repo.createChoice(choicesCopy[c]);
+                    console.log(choicesCopy[c]);
+                }
+                this.router.navigateByUrl("/new/quiz/created");
+            }, response => {
+                this.errorMessage = "Unable to insert quiz.";
+                console.log(this.errorMessage);
+            });
     }
 
     createForm() {
@@ -119,9 +101,13 @@ export class CreateQuizComponent {
         });
     }
 
-    getLastQuiz() {
-        this.sendRequest(RequestMethod.Get, this.repo.urlBase + "api/quizzes/last")
-            .subscribe(response => { this.latestQuiz = response });
+
+    addChoice() {
+        this.choices.push(this.formBuilder.group(new Choice()));
+    }
+
+    removeChoice(index: number) {
+        this.choices.removeAt(index);
     }
 
     increaseTimeLimit() {
@@ -130,13 +116,5 @@ export class CreateQuizComponent {
 
     decreaseTimeLimit() {
         this.quizTimeLimit -= 30;
-    }
-
-    private sendRequest(verb: RequestMethod, url: string, data?: any): Observable<any> {
-        return this.http.request(new Request({
-            method: verb,
-            url: url,
-            body: data
-        })).map(response => response.json());
     }
 }
