b709f23c21906818974d8ab575f48db66eacbe92
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-11-23 12:42:48 +0000

    BUGFIX: Results are now updated correctly. BUGFIX: The participant board component gets destroyed correctly.

--- QuizTime/ClientApp/app/components/joinsession/participantboard.component.ts
+++ QuizTime/ClientApp/app/components/joinsession/participantboard.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Repository } from '../../data/repository';
 import { Quiz } from '../../models/quiz.model';
@@ -6,23 +6,37 @@ import { Session } from '../../models/session.model';
 import { HubConnection } from "@aspnet/signalr-client";
 import { Observable } from "rxjs/Observable";
 import { CookieService } from 'ngx-cookie';
+import { Http } from '@angular/http';
+import "rxjs/add/operator/takeWhile";
 
 @Component({
     selector: 'participant-board',
     templateUrl: './participantboard.component.html'
 })
-export class ParticipantBoardComponent implements OnInit{
+export class ParticipantBoardComponent implements OnInit, OnDestroy{
     private _hubConnection: HubConnection;
     participant = '';
     participants: string[] = [];
     result = '';
+    score = 0;
+    choiceId: number;
     currentSelection: number;
+    errorMessage = "";
+    session: Session = new Session();
+    private alive: boolean = true;
 
     constructor(
         private repo: Repository,
         private router: Router,
         private activeRoute: ActivatedRoute,
-        private cookieService: CookieService) {
+        private cookieService: CookieService,
+        private http: Http) {
+
+        let id = Number.parseInt(this.activeRoute.snapshot.params["id"]);
+
+        if (id < 1)
+            return;
+        this.loadHostedSession(id);
     }
 
     ngOnInit() {
@@ -31,17 +45,24 @@ export class ParticipantBoardComponent implements OnInit{
 
         let id = Number.parseInt(this.activeRoute.snapshot.params["id"]);
 
-        if (id) {
-            this.repo.getHostedSession(id);
-        } else {
-            this.router.navigateByUrl("/");
-        }
-
         this._hubConnection = new HubConnection('/boardhub');
 
         this._hubConnection.on('update', (status: any) => {
-            console.log("Session status became: " + status);
-            this.hostedSession.status = status;
+            console.log("PARTICIPANT: Session status became: " + status);
+            this.loadHostedSession(id);
+            //this.repo.getHostedSession(id);
+            //this.hostedSession.status = status;
+            //if (this.hostedSession != null) {
+            //    this.hostedSession.status = status;
+            //}
+
+            if (status == 4) {
+                this.updateResult();
+            }
+
+            if (status == 5) {
+                this.clearCookies();
+            }
         });
 
         this._hubConnection.start()
@@ -53,18 +74,51 @@ export class ParticipantBoardComponent implements OnInit{
             });
     }
 
-    selectAnswer(choiceId: number, correctness: boolean) {
+    loadHostedSession(id) {
+        this.errorMessage = "";
+        this.http
+            .get(`${this.repo.urlBase}/api/sessions/host/${id}`)
+            .takeWhile(() => this.alive)
+            .subscribe(response => {
+                this.session = response.json();
+                console.log(this.session);
+            }, response => {
+                this.errorMessage = "Unable to load session.";
+            });
+    }
+
+    ngOnDestroy() {
+        console.log("destroyed" + this.session.generatedHostId)
+        this.alive = false;
+        this.repo.alive = false;
+        this.clearCookies();
+    }
+
+    selectAnswer(choiceId: number, correctness: boolean, assignedPoints: number, deducedPoints: number) {
         console.log(correctness);
         this.currentSelection = choiceId;
         if (correctness == true) {
             this.result = "won";
+            this.score = assignedPoints;
+            this.choiceId = choiceId;
         }
         else {
             this.result = "lost";
+            this.score = -deducedPoints;
+            this.choiceId = choiceId;
         }
         this.putCookie("correctness", this.result);
         console.log(this.currentSelection);
         console.log(choiceId);
+        console.log(this.score);
+    }
+
+    updateResult() {
+        let changes = new Map<string, any>();
+        console.log(this.score);
+        changes.set("score", this.score);
+        changes.set("choice", this.choiceId);
+        this.repo.updateResult(parseInt(this.getCookie("resultId")), changes);
     }
 
     getCookie(key: string) {
@@ -75,15 +129,13 @@ export class ParticipantBoardComponent implements OnInit{
         return this.cookieService.put(key, value);
     }
 
-    deleteCookie() {
-        return this.cookieService.remove("correctness");
+    clearCookies() {
+        this.cookieService.remove("correctness");
+        this.cookieService.remove("resultId");
+        this.cookieService.remove("participantName");
     }
-    
-    //checkCookie(key: string) {
-    //    return this.cookieService.check
-    //}
 
-    get hostedSession() {
-        return this.repo.hostedSession;
-    }
+    //get hostedSession() {
+    //    return this.repo.hostedSession;
+    //}
 }
