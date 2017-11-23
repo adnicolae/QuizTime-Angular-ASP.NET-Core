7172c5e3fb6825206b7b48e18ccab5bebbd0f65e
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-11-23 11:18:40 +0000

    BUGFIX: Session board component gets destroyed without memory leaks.

--- QuizTime/ClientApp/app/components/sessionboard/sessionboard.component.ts
+++ QuizTime/ClientApp/app/components/sessionboard/sessionboard.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Repository } from '../../data/repository';
 import { Quiz } from '../../models/quiz.model';
@@ -6,55 +6,54 @@ import { Session } from '../../models/session.model';
 import { HubConnection } from "@aspnet/signalr-client";
 import { Observable } from "rxjs/Observable";
 import { SimpleTimer } from 'ng2-simple-timer';
+import { Http } from '@angular/http';
+import "rxjs/add/operator/takeWhile";
+
 
 @Component({
     selector: 'session-board',
     templateUrl: './sessionboard.component.html'
 })
-export class SessionBoardComponent {
+export class SessionBoardComponent implements OnInit, OnDestroy {
     private _hubConnection: HubConnection;
     participant = '';
     participants: string[] = [];
     currentStatus: number = 1;
-
+    session: Session = new Session();
+    errorMessage = "";
     timer1Id: string;
     timer2Id: string;
     counter1 = 10;
     counter2: number;
+    private alive: boolean = true;
 
 
     constructor(
         private repo: Repository,
         private router: Router,
         private activeRoute: ActivatedRoute,
-        private timer: SimpleTimer) {
+        private timer: SimpleTimer,
+        private http: Http) {
+
+        let id = Number.parseInt(this.activeRoute.snapshot.params["id"]);
+
+        if (id < 1)
+            return;
+        this.loadHostedSession(id);
     }
 
     ngOnInit() {
         let id = Number.parseInt(this.activeRoute.snapshot.params["id"]);
 
-        if (id) {
-            this.repo.getHostedSession(id);
-        } else {
-            this.router.navigateByUrl("/");
-        }
-
         this._hubConnection = new HubConnection('/boardhub');
 
         this._hubConnection.on('send', (username: any) => {
-            console.log("Added " + username + " to array");
-            this.repo.getHostedSession(id);
+            this.loadHostedSession(id);
         });
 
         this._hubConnection.on('update', (status: any) => {
-            console.log("Session status became: " + status);
-            if (this.hostedSession != null) {
-                this.hostedSession.status = status;
-            }
-        });
-
-        this._hubConnection.on('send', username => {
-            console.log(username);
+            console.log("HOST: Session status became: " + status);
+            this.loadHostedSession(id);
         });
 
         this._hubConnection.start()
@@ -67,6 +66,27 @@ export class SessionBoardComponent {
 
         this.timer.newTimer('1sec', 1);
         this.timer.newTimer('1sec2', 1);
+    }
+
+    ngOnDestroy() {
+        console.log("destroyed " + this.session.generatedHostId)
+        this.alive = false;
+        this.repo.alive = false;
+        this.timer.unsubscribe(this.timer1Id);
+        this.timer.unsubscribe(this.timer2Id);
+    }
+
+    loadHostedSession(id) {
+        this.errorMessage = "";
+        this.http
+            .get(`${this.repo.urlBase}/api/sessions/host/${id}`)
+            .takeWhile(() => this.alive)
+            .subscribe(response => {
+                this.session = response.json();
+                console.log(this.session);
+            }, response => {
+                this.errorMessage = "Unable to load session.";
+            });
     }
 
     subscribeTimer1() {
@@ -99,7 +119,8 @@ export class SessionBoardComponent {
         this.counter1--;
 
         if (this.counter1 == 0) {
-            this.updateSession(0);
+            this.timer.unsubscribe(this.timer1Id);
+            this.updateSessionWithNoTimer();
             this.subscribeTimer2();
         }
     }
@@ -108,14 +129,15 @@ export class SessionBoardComponent {
         this.counter2--;
 
         if (this.counter2 == 0) {
-            this.updateSession(0);
+            this.timer.unsubscribe(this.timer2Id);
+            this.updateSessionWithNoTimer();
         }
     }
 
     updateSession(timeLimit: number) {
         let changes = new Map<string, any>();
-        changes.set("status", this.currentStatus+1);
-        this.repo.updateSession(this.hostedSession.sessionId as number, changes);
+        changes.set("status", this.currentStatus + 1);
+        this.repo.updateSession(this.session.sessionId as number, changes);
         this.currentStatus++;
         if (timeLimit != 0) {
             this.counter2 = timeLimit;
@@ -126,16 +148,16 @@ export class SessionBoardComponent {
     updateSessionWithNoTimer() {
         let changes = new Map<string, any>();
         changes.set("status", this.currentStatus + 1);
-        this.repo.updateSession(this.hostedSession.sessionId as number, changes);
+        this.repo.updateSession(this.session.sessionId as number, changes);
         this.currentStatus++;
     }
 
-    get hostedSession(): Session {
-        return this.repo.hostedSession;
-    }
+    //get hostedSession(): Session {
+    //    return this.repo.hostedSession;
+    //}
 
 
-    get quiz(): Quiz {
-        return this.repo.quiz;
-    }
+    //get quiz(): Quiz {
+    //    return this.repo.quiz;
+    //}
 }
