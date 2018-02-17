373906b8b5906da561976f1c993768119d3f21ec
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-02-17 14:22:09 +0200

    Updated home page UI.

--- QuizTime/ClientApp/app/components/home/home.component.ts
+++ QuizTime/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Repository } from '../../data/repository';
+import { Result } from '../../models/result.model';
 import { Session } from '../../models/session.model';
 import { HubConnection } from "@aspnet/signalr-client";
 import { Observable } from "rxjs/Observable";
+import { AuthService } from '../registration/auth.service';
+import { isPlatformServer, isPlatformBrowser } from '@angular/common';
 
 @Component({
     selector: 'home',
@@ -14,10 +17,14 @@ export class HomeComponent implements OnInit {
     public async: any;
     message = '';
     messages: string[] = [];
+    isBrowser: boolean
 
-
-    constructor(private repo: Repository) {
-        this.repo.getHostedSession(3456);
+    constructor(@Inject(PLATFORM_ID) private platformId: Object, private repo: Repository, public auth: AuthService) {
+        //this.repo.getHostedSession(3456);
+        this.isBrowser = isPlatformBrowser(platformId);
+        if (this.isBrowser && this.auth.isAuthenticated) {
+            this.repo.getParticipantResults(5);
+        }
     }
 
     public sendMessage(): void {
@@ -50,5 +57,9 @@ export class HomeComponent implements OnInit {
 
     get hostedSession(): Session {
         return this.repo.hostedSession;
+    }
+
+    get participantResults(): Result[] {
+        return this.repo.participantResults;
     }
 }
