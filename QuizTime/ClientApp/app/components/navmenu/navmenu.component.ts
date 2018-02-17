ca3a0d70c73df0dfc6ed4d9b4d85b704fe80b876
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-02-17 14:27:14 +0200

    Preliminary nav menu style update.

--- QuizTime/ClientApp/app/components/navmenu/navmenu.component.ts
+++ QuizTime/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformServer, isPlatformBrowser } from '@angular/common';
+import { AuthService } from '../registration/auth.service';
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
     styleUrls: ['./navmenu.component.css']
 })
+
 export class NavMenuComponent {
+    isBrowser: boolean;
+
+    constructor( @Inject(PLATFORM_ID) private platformId: Object, public auth: AuthService) {
+        this.isBrowser = isPlatformBrowser(platformId);
+    }
 }
