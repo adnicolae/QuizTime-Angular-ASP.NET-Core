09c43610abc635a9587feaa5536971a13c25f69c
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-02-17 14:12:30 +0200

    Added authentication through tokens service.

---
+++ QuizTime/ClientApp/app/components/registration/auth.service.ts
@@ -0,0 +1,56 @@
+﻿import { Injectable, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router'
+
+@Injectable()
+
+export class AuthService {
+
+    //baseUrl = "http://localhost:61886/auth/register";
+    baseUrl;
+    usernameKey = 'username';
+    tokenKey = 'token';
+    localStorage: any;
+
+    constructor(private http: Http, @Inject('BASE_URL') baseUrl: string, private router: Router) {
+        this.baseUrl = baseUrl;
+    }
+
+    get name() {
+        return localStorage.getItem(this.usernameKey);
+    }
+
+    get isAuthenticated() {
+        return !!localStorage.getItem(this.tokenKey);
+    }
+
+    register(user) {
+        delete user.passwordConfirmation;
+        this.http.post(this.baseUrl + '/auth/register', user)
+            .subscribe(response => {
+                this.authenticate(response);
+            });
+        console.log(this.baseUrl);
+    }
+
+    logout() {
+        localStorage.removeItem(this.tokenKey);
+        localStorage.removeItem(this.usernameKey);
+    }
+
+    login(loginData) {
+        this.http.post(this.baseUrl + 'auth/login', loginData).subscribe(response => { this.authenticate(response); })
+    }
+
+    authenticate(response) {
+        var authenticationResponse = response.json();
+
+        if (!authenticationResponse.token)
+            return;
+
+        localStorage.setItem(this.tokenKey, authenticationResponse.token)
+        localStorage.setItem(this.usernameKey, authenticationResponse.username)
+
+        this.router.navigate(['/'])
+    }
+}
