385650606addf8f2de99682439349328e0e99234
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-02-17 14:13:52 +0200

    Added login component.

---
+++ QuizTime/ClientApp/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../registration/auth.service';
+
+@Component({
+    selector: 'login',
+    templateUrl: 'login.component.html',
+    styles: [`
+        .error {
+            background-color: #fff0f0
+        }
+    `
+    ]
+})
+
+export class LoginComponent {
+    constructor(private auth: AuthService) { }
+
+    loginData = {
+        username: '',
+        password: ''
+    }
+
+    login() {
+        this.auth.login(this.loginData);
+    }
+}
