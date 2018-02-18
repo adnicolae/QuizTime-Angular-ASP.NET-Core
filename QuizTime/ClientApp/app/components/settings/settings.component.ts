1652e467672e3f2c925e87d3e3c659586041a5a5
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-02-18 17:05:00 +0200

    Added Settings component.

---
+++ QuizTime/ClientApp/app/components/settings/settings.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../registration/auth.service';
+
+@Component({
+    selector: 'settings',
+    templateUrl: 'settings.component.html',
+    styles: [`
+        .error {
+            background-color: #fff0f0
+        }
+    `
+    ]
+})
+
+export class SettingsComponent {
+    constructor(private auth: AuthService) { }
+
+    settingsData = {
+        defaultQuizTitle: ''
+    }
+
+}
