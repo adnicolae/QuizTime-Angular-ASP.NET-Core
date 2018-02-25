14d4a1caa1b88ad9e33957264243eacf4f0a729e
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-02-25 19:46:28 +0200

    Servicing fetch quizzes. - to be updated.

--- QuizTime/ClientApp/app/components/fetchquizzes/fetchquizzes.component.ts
+++ QuizTime/ClientApp/app/components/fetchquizzes/fetchquizzes.component.ts
@@ -1,5 +1,7 @@
 ﻿import { Component, Inject, OnDestroy } from "@angular/core";
+import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 import { Quiz } from "../../models/quiz.model";
+import { Group } from "../../models/group.model";
 import { Session } from "../../models/session.model";
 import { Http } from '@angular/http';
 import { Repository } from "../../data/repository";
@@ -22,74 +24,29 @@ export class FetchQuizzesComponent {
 
     constructor(private repo: Repository, private router: Router, private http: Http, @Inject('BASE_URL') baseUrl: string) {
         this.baseUrl = baseUrl;
+        this.repo.getGroup(7);
+
     }
 
     get quiz(): Quiz {
         return this.repo.quiz;
     }
 
-    get quizzes(): Quiz[] {
-        if (this.repo.quizzes != null) {
-            return this.repo.quizzes.reverse();
-        }
-        else {
-            return this.repo.quizzes;
-        }
-    }
-
-    get hostedSession(): Session {
-        return this.repo.hostedSession;
-    }
-
-    get sessions(): Session[] {
-        return this.repo.sessions;
-    }
-
-    selectQuiz(quizz: Quiz) {
-        // generate random session id 
-        var generatedId: number = parseInt(this.generatePin());
-        //console.log(this.sessions.map(s => JSON.stringify(s.quiz)));
-        //console.log(JSON.stringify(quizz));
-        //TODO: add verification if a session for that quiz is already in status 1, then don't create a new one
-        if (JSON.stringify(this.quiz) === JSON.stringify(quizz)) {
-            console.log("already a session with quiz");
-        } else {
-            // create a new session
-            //this.repo.createSession(new Session(0, new Date(), generatedId, 1, quizz));
-
-            let data = {
-                dateCreated: new Date(),
-                generatedHostId: generatedId,
-                status: 1,
-                quiz: quizz ? quizz.quizId : 0
-            };
-
-            let url = this.baseUrl + sessionsUrl;
-
-            this.http
-                .post(url, data)
-                .takeWhile(() => this.alive)
-                .subscribe(response => {
-                    // go to "/session-board/host/{id}"
-                    this.router.navigateByUrl("/session-board/host/" + generatedId);
-                });
-        }
-    }
-
-    // handle session insertion
-    logSession(quiz: Quiz) {
-        console.log(quiz.quizId);
-        console.log(this.generatePin());
-    }
-
-    generatePin() {
-        var min = 1000;
-        var max = 99999;
-        return ("0" + (Math.floor(Math.random() * (max - min + 1)) + min)).substr(-4);
-    }
+    //get quizzes(): Quiz[] {
+    //    if (this.repo.quizzes != null) {
+    //        return this.repo.quizzes.reverse();
+    //    }
+    //    else {
+    //        return this.repo.quizzes;
+    //    }
+    //}
 
     ngOnInit() {
         this.repo.alive = true;
+    }
+
+    get groups() {
+        return this.repo.group;
     }
 
     ngOnDestroy() {
@@ -97,4 +54,5 @@ export class FetchQuizzesComponent {
         console.log("Repo set to alive");
         this.repo.alive = false;
     }
+
 }
