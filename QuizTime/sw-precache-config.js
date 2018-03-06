bcd69be3b812156c12bca0f56d468c3aad65cdf8
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2018-03-07 01:17:36 +0200

    Added service worker pre-caching.

---
+++ QuizTime/sw-precache-config.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+    staticFileGlobs: [
+        'Views/**.cshtml',
+        'Views/Home/**.cshtml',
+        'Views/Shared/**.cshtml',
+        'wwwroot/**.js',
+        'wwwroot/**.css',
+        'wwwroot/**.png',
+        'wwwroot/dist/**.js',
+        'wwwroot/dist/**.css',
+        'wwwroot/dist/**.png',
+        'wwwroot/dist/dist/**.eot',
+        'wwwroot/dist/dist/**.woff2',
+        'wwwroot/dist/dist/**.svg',
+        'wwwroot/dist/dist/**.ttf',
+        'wwwroot/dist/dist/**.woff',
+        'wwwroot/sounds/**.wav',
+        'wwwroot/sounds/**.mp3',
+    ],
+    root: 'wwwroot/dist',
+    stripPrefix: 'wwwroot/dist/',
+    navigateFallback: 'Views/Home/Index'
+};
